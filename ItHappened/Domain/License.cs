8626753e79634466bfcd97efc58e5c04093820ba
Arthur Khazbulatov <atkhazbulatov@edu.hse.ru>
2020-10-14 16:16:05 +0300

    Add License, User model

---
+++ ItHappened/Domain/License.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ItHappened.Domain
+{
+    public readonly struct License
+    {
+        public enum LicenseType
+        {
+            Free,
+            Premium
+        }
+
+        public LicenseType Type { get; }
+        public DateTime ExpiryDate { get; }
+
+        public License(LicenseType type, DateTime expiryDate)
+        {
+            Type = type;
+            ExpiryDate = expiryDate;
+        }
+    }
+}
