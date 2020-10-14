b4c4f1ff99eece3d23b3b9cedddca030849791be
silkslime <silkslime@yandex.ru>
2020-10-14 15:51:22 +0300

    PR comments resolved

--- ItHappened/Domain/Event.cs
+++ ItHappened/Domain/Event.cs
@@ -6,27 +6,17 @@ namespace ItHappened.Domain
     {
         public Guid Id { get; }
         public Guid TrackerId { get; }
-        public string Title { get; private set; }
+        public string Title { get; set; }
         public DateTime CreationDate { get; }
-        public DateTime ChangingDate { get; private set; }
+        public DateTime ModificationDate { get; set; }
 
-        public Event(Guid id, Guid trackerId, string title, DateTime creationDate, DateTime changingDate)
+        public Event(Guid id, Guid trackerId, string title, DateTime creationDate, DateTime modificationDate)
         {
             Id = id;
             TrackerId = trackerId;
             Title = title ?? throw new ArgumentNullException(nameof(title));
             CreationDate = creationDate;
-            ChangingDate = changingDate;
-        }
-
-        public void SetTitle(string title)
-        {
-            Title = title;
-        }
-        
-        public void SetChangingDate(DateTime changingDate)
-        {
-            ChangingDate = changingDate;
+            ModificationDate = modificationDate;
         }
     }
 }
