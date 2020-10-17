cf174928ad454db2e046aed130a39af6e52cc48d
Arthur Khazbulatov <atkhazbulatov@edu.hse.ru>
2020-10-17 22:01:19 +0300

    Add user service, user form and hasher interface

---
+++ ItHappened/Application/UserForm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using LanguageExt;
+
+namespace ItHappened.Application
+{
+    public class UserForm
+    {
+        public Option<Guid> Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ ItHappened/Application/UserService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ItHappened.Domain;
+using ItHappened.Infrastructure;
+using LanguageExt;
+using Serilog;
+
+namespace ItHappened.Application
+{
+    public class UserService
+    {
+        private IRepository<User> _userRepository;
+        private IHasher _hasher;
+
+        public UserService(IRepository<User> userRepository) =>
+            _userRepository = userRepository;
+
+        public IEnumerable<User> GetAll() =>
+            _userRepository.GetAll();
+
+        public Option<User> GetById(Guid userId) =>
+            _userRepository.Get(userId);
+
+        public Option<User> GetByUsername(string username) =>
+            _userRepository.GetAll().SingleOrDefault(user => user.Username == username);
+
+        public Option<User> CreateUser(UserForm userForm)
+        {
+            string intent = $"create new user named '{userForm.Username}'";
+            if (GetByUsername(userForm.Username).IsSome)
+            {
+                Log.Information($"Failed: {intent} - username taken");
+                return Option<User>.None;
+            }
+            
+            User user = new User(Guid.NewGuid(), userForm.Username, _hasher.MakeSaltedHash(userForm.Password),
+                new License(LicenseType.Free, DateTime.MaxValue), DateTime.Now, DateTime.Now);
+            
+            _userRepository.Save(user);
+            Log.Information($"Success: {intent}");
+            return user;
+        }
+
+        public void SetPassword(Guid principalId, Guid userId, string oldPassword, string newPassword)
+        {
+            string intent = $"modify password for user '{userId}'";
+            Option<User> modifiedUser = GetById(userId);
+            if (!modifiedUser.Exists(user => 
+                    user.Id == principalId 
+                    && _hasher.VerifySaltedHash(oldPassword, user.PasswordHash)))
+                Log.Information($"Access denied: {intent}");
+
+            modifiedUser
+                .Do(user => user.PasswordHash = _hasher.MakeSaltedHash(newPassword))
+                .Do(user => user.ModificationDate = DateTime.Now);
+            Log.Information($"Success: {intent}");
+        }
+
+        public Option<User> LogInByCredentials(string username, string password)
+        {
+            string intent = $"log in as user named '{username}'";
+            Option<User> user = GetByUsername(username);
+            if (!user.Exists(u => _hasher.VerifySaltedHash(password, u.PasswordHash)))
+            {
+                Log.Information($"Access denied: {intent}");
+                return Option<User>.None;
+            }
+            return user;
+        }
+    }
+}
