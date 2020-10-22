694177619f34a92fb325bd3e580a71179ca1b5e9
iliazen <iliazenin.msu@gmail.com>
2020-10-22 22:42:29 +0300

    TrackerService refatoring completed (tochno) and unit tests added

--- ItHappened/Application/TrackerService.cs
+++ ItHappened/Application/TrackerService.cs
@@ -16,25 +16,32 @@ namespace ItHappened.Application
         }
         public void CreateTracker(Guid actorId, TrackerForm form)
         {
-            var tracker = new Tracker(Guid.NewGuid(), actorId, form.Title, DateTime.Now, 
-                DateTime.Now, form.Customizations);
-            _trackersRepository.Save(tracker);
+            if (form.IsNull())
+            {
+                var tracker = new Tracker(Guid.NewGuid(), actorId, form.Title, DateTime.Now,
+                    DateTime.Now, form.Customizations);
+                _trackersRepository.Save(tracker);
+            }
         }
 
         public void EditTracker(Guid actorId, Guid trackerId, TrackerForm form)
         {
-            var oldTracker = _trackersRepository.Get(trackerId);
-            oldTracker.Do(tracker =>
+            if (form.IsNull())
             {
-                if (actorId != tracker.UserId)
+                var oldTracker = _trackersRepository.Get(trackerId);
+                oldTracker.Do(tracker =>
                 {
-                    Log.Error($"User {actorId} tried to edit someone else's tracker");
-                    return;
-                }
-                var newTracker = new Tracker(tracker.Id, tracker.UserId, form.Title,
-                    tracker.CreationDate, DateTime.Now, form.Customizations);
-                _trackersRepository.Update(newTracker);
-            });
+                    if (actorId != tracker.UserId)
+                    {
+                        Log.Error($"User {actorId} tried to edit someone else's tracker");
+                        return;
+                    }
+
+                    var newTracker = new Tracker(tracker.Id, tracker.UserId, form.Title,
+                        tracker.CreationDate, DateTime.Now, form.Customizations);
+                    _trackersRepository.Update(newTracker);
+                });
+            }
         }
 
         public void DeleteTracker(Guid actorId, Guid trackerId)
