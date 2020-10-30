6225640ddb3dd1a6cf48da5a0ef56e89d606cf45
Arthur Khazbulatov <atkhazbulatov@edu.hse.ru>
2020-10-31 00:42:03 +0300

    Add DateTimeExtensions (+ tests) to work with weeks

---
+++ ItHappened/Domain/Stats/DateTimeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ItHappened.Domain.Stats
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime GetLast(this DateTime date, DayOfWeek day) =>
+            date.AddDays(-(DaysInWeek - (int) day + (int) date.DayOfWeek) % DaysInWeek);
+
+        public static DateTime GetNext(this DateTime date, DayOfWeek day) =>
+            date.AddDays(+(DaysInWeek + (int) day - (int) date.DayOfWeek) % DaysInWeek);
+
+        private static readonly int DaysInWeek = Enum.GetNames(typeof(DayOfWeek)).Length;
+    }
+}
