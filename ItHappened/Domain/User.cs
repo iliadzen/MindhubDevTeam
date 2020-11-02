37383dc6b2ac9944d8e1ebf4470f967414a2725f
silkslime <silkslime@yandex.ru>
2020-11-02 17:52:53 +0300

    Edit User Domain model to private setters

--- ItHappened/Domain/User.cs
+++ ItHappened/Domain/User.cs
@@ -4,11 +4,11 @@ namespace ItHappened.Domain
 {
     public class User : IEntity
     {
-        public Guid Id { get; set; }
+        public Guid Id { get; private set; }
         public string Username { get; set; }
         public string PasswordHash { get; set; }
-        public DateTime CreationDate { get; set; }
-        public DateTime ModificationDate { get; set; }
+        public DateTime CreationDate { get; private set; }
+        public DateTime ModificationDate { get; private set; }
 
         public User(Guid id, string username, string passwordHash,
             DateTime creationDate, DateTime modificationDate)
